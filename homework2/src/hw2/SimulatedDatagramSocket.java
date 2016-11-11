db3b2f121b9fc92a2495cbd111346e253bd542fe
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-11 21:18:44 +0100

    [hw2:socket] Create simulated socket

---
+++ homework2/src/hw2/SimulatedDatagramSocket.java
@@ -0,0 +1,59 @@
+/*
+ * This code has been developed at Department of Telecommunications,
+ * Faculty of Electrical Engineering and Computing, University of Zagreb.
+ */
+package hw2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class SimulatedDatagramSocket extends DatagramSocket {
+
+	private final double lossRate;
+	private final int averageDelay;
+
+	public static DatagramSocket client(double lossRate, int averageDelay)
+			throws SocketException {
+		return new SimulatedDatagramSocket(0, lossRate, averageDelay, 4 * averageDelay);
+	}
+
+	public static DatagramSocket server(int port, double lossRate, int averageDelay)
+			throws SocketException {
+		return new SimulatedDatagramSocket(port, lossRate, averageDelay, 0);
+	}
+
+	private SimulatedDatagramSocket(int port, double lossRate, int averageDelay, int soTimeout)
+			throws SocketException {
+		super(port);
+		this.lossRate = lossRate;
+		this.averageDelay = averageDelay;
+		super.setSoTimeout(soTimeout);
+	}
+
+	@Override
+	public void send(DatagramPacket packet) throws IOException {
+
+		if (Util.RANDOM.nextDouble() >= lossRate) {
+
+			//delay is uniformly distributed between 0 and 2*averageDelay
+			long delay = (long) (2 * averageDelay * Util.RANDOM.nextDouble());
+
+			new Thread(() -> {
+				try {
+					Thread.sleep(delay);
+					SimulatedDatagramSocket.super.send(packet);
+				} catch (InterruptedException e) {
+					Thread.interrupted();
+				} catch (IOException ex) {
+					Logger.getLogger(SimulatedDatagramSocket.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+			).start();
+
+		}
+	}
+}
