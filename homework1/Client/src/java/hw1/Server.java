bf094d87dc285a8d94b35c9c90a19074bf6751a7
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-11 20:42:12 +0100

    [hw1:Client] Remove main from Server.java

--- homework1/Client/src/java/hw1/Server.java
+++ homework1/Client/src/java/hw1/Server.java
@@ -16,13 +16,6 @@ public class Server {
         service = new hw1.ServerService_Service().getServerPort();
     }
 
-    public static void main(String[] args) {
-        register("a", 0, 0, "b", 0);
-        register("c", 1, 1, "d", 1);
-        System.out.println(searchNeighbour("a"));
-        System.out.println(storeMeasurement("a", "metan", 10));
-    }
-
     public static boolean register(java.lang.String username, double latitude, double longitude, java.lang.String ipaddress, int port) {
         return service.register(username, latitude, longitude, ipaddress, port);
     }
