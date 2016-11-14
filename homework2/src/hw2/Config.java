5e4096b88967291fcf7a0ee95a78a9b94d3e28fe
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-14 19:40:53 +0100

    [hw2:config] remove unused method

--- homework2/src/hw2/Config.java
+++ homework2/src/hw2/Config.java
@@ -6,15 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-/**
- * Created by fhrenic on 11/11/2016.
- */
 class Config {
 
 	private static final int NUM_OF_NODES = 4;
 	private static final Map<String, InetSocketAddress> NODES;
-
-
+	
 	static final double NETWORK_LOSS_RATE = 0.2;
 	static final int NETWORK_AVERAGE_DELAY = 1000;
 
@@ -27,10 +23,6 @@ class Config {
 		for (int i = 0; i < NUM_OF_NODES; i++) {
 			NODES.put("node" + i, new InetSocketAddress("127.0.0.1", 10000 + i));
 		}
-	}
-
-	public static int getNumberOfNodes() {
-		return NUM_OF_NODES;
 	}
 
 	static Iterable<String> getNodeNames() {
