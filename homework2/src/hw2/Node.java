a3717c6fd1f8b646ae497ccc55a9541dbaf2ab27
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-14 20:49:24 +0100

    [hw2:node] finished node

--- homework2/src/hw2/Node.java
+++ homework2/src/hw2/Node.java
@@ -1,48 +1,52 @@
 package hw2;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+/**
+ * Represents one node in a distributed environment.
+ */
 public class Node {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-//		if (args.length != 1) {
-//			throw new IllegalArgumentException("Expecting only one argument - node name");
-//		}
-//		String name = args[0];
-//
-//		Node node = new Node(name);
+	/**
+	 * Start a new node
+	 *
+	 * @param args expecting only one
+	 */
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			throw new IllegalArgumentException("Expecting only one argument - node name");
+		}
+		String name = args[0];
 
-		new Thread(() -> {
-			try {
-				new Node("node1").bbb();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}).start();
-		Thread.sleep(1123);
-		new Node("node0").aaa();
+		try {
+			new Node(name).start();
+		} catch (SocketException e) {
+			Util.debug("Socket error for %s", name);
+		} catch (IllegalArgumentException e) {
+			Util.debug("Unknown name for node: %s", name);
+		}
 
 	}
 
 	private final String name;
+	private final long startTime;
+
 	private final DatagramSocket sender;
 	private final DatagramSocket receiver;
+	private final Iterable<SocketAddress> nodes;
+	private final Map<SocketAddress, Long> lastNum;
 
-	private final long startTime;
-	private final EmulatedSystemClock esc;
-	private Mark mark;
-
+	private final Mark mark;
 	private final List<Packet> memory;
 	private final List<Double> co2;
 
-	private Node(String name) throws SocketException {
+	private final EmulatedSystemClock esc;
+
+	private Node(String name) throws SocketException, IllegalArgumentException {
 
 		this.name = name;
 		InetSocketAddress addr = Config.getAddressFor(name);
@@ -60,49 +64,67 @@ public class Node {
 			sender = SimulatedDatagramSocket.client(lossRate, averageDelay);
 			receiver = SimulatedDatagramSocket.server(addr.getPort(), lossRate, averageDelay);
 		} catch (SocketException ex) {
-			Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+			Util.except(ex);
 			throw ex;
 		}
 
-		mark = new Mark(startTime);
-		memory = new LinkedList<>();
+		mark = new Mark(startTime, name);
+		nodes = Config.getNodesFor(name);
+		memory = Collections.synchronizedList(new LinkedList<>());
+		lastNum = Collections.synchronizedMap(new HashMap<>());
 		co2 = Util.readCO2();
 
 		Util.debug("Created new node [%s, %d, %d]", name, addr.getPort(), startTime);
 	}
 
-	private void aaa() throws IOException {
-		Payload p = new Payload(0);
-		for (int i = 0;i<5;i++){
-			p.setCo2(i);
-			DatagramPacket dp = Packet.create(null, p).toDatagram(new InetSocketAddress("localhost", 10001));
-			sender.send(dp);
+	private void start() {
+
+		// start receiving packets
+		new Thread(new Receiver()).start();
+
+		// send packets every minute
+		while (true) {
+			for (int i = 0; i < Config.LOOP_ITERATIONS; i++) {
+				try {
+					TimeUnit.SECONDS.sleep(Config.EVERY_N_SECONDS);
+					iteration();
+				} catch (InterruptedException e) {
+					Util.except(e);
+				}
+			}
+			output();
 		}
 	}
 
-	private void bbb() throws IOException {
-		byte[] buff = new byte[8192];
-		DatagramPacket dp = new DatagramPacket(buff, buff.length);
-		while (true) {
-			receiver.receive(dp);
-			Packet p = Packet.fromDatagram(dp);
-			System.out.println("RECIEVED " + p);
+	private void iteration() {
+		// create payload with current measurement
+		Payload payload = new Payload(getMeasurement());
+
+		// onSend to all nodes
+		for (SocketAddress sa : nodes) {
+			mark.onSend(name);
+			Packet packet = Packet.create(lastNum.getOrDefault(sa, 0L), mark, payload);
+			try {
+				sender.send(packet.toDatagram(sa));
+			} catch (IOException e) {
+				Util.except(e);
+			}
 		}
 	}
 
 	private void output() {
-
-		Collection<Packet> packets = new LinkedList<>(memory);
-		memory.clear();
+		List<Packet> packets;
+		synchronized (this) {
+			packets = new LinkedList<>(memory);
+			memory.clear();
+		}
 
 		String scalarSort = sortPackets(
-				packets, "scalar",
-				(p1, p2) -> p1.getMark().compareScalar(p2.getMark())
+				packets, (p1, p2) -> p1.getMark().compareScalar(p2.getMark())
 		);
 
 		String vectorSort = sortPackets(
-				packets, "vector",
-				(p1, p2) -> p1.getMark().getVector().get(name).compareTo(p2.getMark().getVector().get(name))
+				packets, (p1, p2) -> p1.getMark().compareVector(p2.getMark())
 		);
 
 		double average = packets.parallelStream()
@@ -110,29 +132,84 @@ public class Node {
 				.average().orElse(0.0);
 
 		System.out.println(
-				new StringJoiner("\n").add(scalarSort).add(vectorSort).add(name + " average=" + average)
+				new StringJoiner("\n\t")
+						.add("> " + name)
+						.add(scalarSort)
+						.add(vectorSort)
+						.add(String.format("average=%.2f", average))
 		);
 	}
 
-	private String sortPackets(Collection<Packet> memory, String sortType, Comparator<Packet> comp) {
-		Set<Packet> packets = new TreeSet<>(comp);
-		packets.addAll(memory);
-		StringJoiner sj = new StringJoiner(", ", name + " " + sortType, "");
-		packets.forEach(p -> sj.add(p.getPayload().toString()));
-		return sj.toString();
+	private String sortPackets(List<Packet> memory, Comparator<Packet> comp) {
+		Collections.sort(memory, comp);
+		return memory.stream()
+				.map(p -> p.getPayload().toString())
+				.collect(Collectors.joining(",", "[", "]"));
 	}
 
-	private void onRecieve(Packet packet) {
-		mark.recieve(name, packet.getMark());
-	}
-
-	private void onComputeOrSend() {
-		mark.computeOrSend(name);
-	}
-
-	private double getRandomMeasurement() {
-		int idx = (int) ((esc.currentTimeMillis() - startTime) % co2.size());
+	private double getMeasurement() {
+		int idx = Math.abs((int) (esc.currentTimeMillis() - startTime)) % co2.size();
 		return co2.get(idx);
+	}
+
+	private class Receiver implements Runnable {
+
+		private static final int BUFFER_SIZE = 8192;
+
+		@Override
+		public void run() {
+
+			byte[] buff = new byte[BUFFER_SIZE];
+			DatagramPacket dp = new DatagramPacket(buff, buff.length);
+			Packet packet;
+
+			while (true) {
+
+				try {
+					Node.this.receiver.receive(dp);
+				} catch (SocketTimeoutException e) {
+					Util.except(e);
+					System.err.println("Receive timeout on " + Node.this.name);
+					continue;
+				} catch (IOException e) {
+					Util.except(e);
+					continue;
+				}
+
+				SocketAddress sa = dp.getSocketAddress();
+
+				Util.debug("Received packet on %s", Node.this.name);
+				try {
+					packet = Packet.fromDatagram(dp);
+				} catch (IOException | ClassNotFoundException e) {
+					Util.except(e);
+					continue;
+				}
+
+				Mark other = packet.getMark();
+				if (other == null) {
+					continue;
+				}
+				mark.onReceive(Node.this.name, other);
+
+				if (packet.getPayload() == null) {
+					// ack
+					// update last num
+					lastNum.compute(sa, (s, l) -> l == null ? 1 : l + 1);
+				} else {
+					// data
+					// save packet and onSend ack
+					memory.add(packet);
+					mark.onSend(Node.this.name);
+					try {
+						sender.send(Packet.ack(packet.getNum(), mark).toDatagram(dp.getSocketAddress()));
+					} catch (IOException e) {
+						Util.except(e);
+					}
+				}
+
+			}
+		}
 	}
 
 }
