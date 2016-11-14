89281f1d457769904622c671be6aaf2e94d4bec9
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-14 19:44:14 +0100

    [hw2:payload] remove main from payload, remove public modifier, remove method

--- homework2/src/hw2/Payload.java
+++ homework2/src/hw2/Payload.java
@@ -2,13 +2,7 @@ package hw2;
 
 import java.io.Serializable;
 
-/**
- * Created by fhrenic on 11/11/2016.
- */
-public class Payload implements Serializable {
-	public static void main(String[] args) {
-		System.out.println(new Payload(25.123213));
-	}
+class Payload implements Serializable {
 
 	private double co2;
 
@@ -16,12 +10,8 @@ public class Payload implements Serializable {
 		this.co2 = co2;
 	}
 
-	public double getCo2() {
+	double getCo2() {
 		return co2;
-	}
-
-	public void setCo2(double co2) {
-		this.co2 = co2;
 	}
 
 	@Override
