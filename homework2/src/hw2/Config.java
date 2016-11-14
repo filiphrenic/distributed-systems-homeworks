393c297e726e8ccb8193e3658f88b63e68ecb181
Filip Hrenić <hrenic.filip@gmail.com>
2016-11-14 19:40:19 +0100

    [hw2:config] add constants and change method signature

--- homework2/src/hw2/Config.java
+++ homework2/src/hw2/Config.java
@@ -1,6 +1,7 @@
 package hw2;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,8 +14,13 @@ class Config {
 	private static final int NUM_OF_NODES = 4;
 	private static final Map<String, InetSocketAddress> NODES;
 
+
 	static final double NETWORK_LOSS_RATE = 0.2;
 	static final int NETWORK_AVERAGE_DELAY = 1000;
+
+	// Repeat process every-N-seconds for loop-iterations times
+	static final int EVERY_N_SECONDS = 1;
+	static final int LOOP_ITERATIONS = 5;
 
 	static {
 		NODES = new HashMap<>();
@@ -35,7 +41,7 @@ class Config {
 		return NODES.get(nodeName);
 	}
 
-	public static Iterable<InetSocketAddress> getNodesFor(String nodeName) {
+	static Iterable<SocketAddress> getNodesFor(String nodeName) {
 		return NODES.entrySet().parallelStream()
 				.filter(e -> !e.getKey().equals(nodeName))
 				.map(Map.Entry::getValue)
